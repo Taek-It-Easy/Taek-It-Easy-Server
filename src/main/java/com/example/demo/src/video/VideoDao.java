39f89564a9eb34f2d9c9fbe346368b0067510c78
고다은 <2021057301>
2024-02-11 14:52:26 +0900

    feat: Add poseIdx in video streaming API

--- src/main/java/com/example/demo/src/video/VideoDao.java
+++ src/main/java/com/example/demo/src/video/VideoDao.java
@@ -17,10 +17,11 @@ public class VideoDao {
 
     // 해당 userIdx를 갖는 유저조회
     public GetPoseVideoRes getPoseVideo(int poseIdx) {
-        String getPoseVideoQuery = "select poseName, url from Pose LEFT JOIN PoseVideo b ON b.poseIdx = Pose.poseIdx where b.poseIdx = ?;"; // 해당 userIdx를 만족하는 유저를 조회하는 쿼리문
+        String getPoseVideoQuery = "select b.poseIdx, poseName, url from Pose LEFT JOIN PoseVideo b ON b.poseIdx = Pose.poseIdx where b.poseIdx = ?;"; // 해당 userIdx를 만족하는 유저를 조회하는 쿼리문
         int getPoseVideoParam = poseIdx;
         return this.jdbcTemplate.queryForObject(getPoseVideoQuery,
                 (rs, rowNum) -> new GetPoseVideoRes(
+                        rs.getInt("poseIdx"),
                         rs.getString("poseName"),
                         rs.getString("url")), // RowMapper(위의 링크 참조): 원하는 결과값 형태로 받기
                 getPoseVideoParam); // 한 개의 회원정보를 얻기 위한 jdbcTemplate 함수(Query, 객체 매핑 정보, Params)의 결과 반환
--- src/main/java/com/example/demo/src/video/model/GetPoseVideoRes.java
+++ src/main/java/com/example/demo/src/video/model/GetPoseVideoRes.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GetPoseVideoRes {
+    private int poseIdx;
     private String poseName;
     private String url;
 }
